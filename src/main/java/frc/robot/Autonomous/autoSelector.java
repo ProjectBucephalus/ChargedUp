0c296569423d76112d9c8449260ffff921f63e37
Adam <51582041+SashimiDude@users.noreply.github.com>
2023-03-02 20:01:12 +1100

    path                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           f
    hwh

---
+++ src/main/java/frc/robot/Autonomous/autoSelector.java
@@ -0,0 +1,15 @@
+package frc.robot.Autonomous;
+
+public class autoSelector {
+    public enum startPos{
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+    };
+    public enum stepTwo{
+        
+    }
+}
